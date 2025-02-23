8f60314f03ca09f1ea9a2b9c19ba1c8922a123a3
aadarsh360 <aadarshpandey64@gmail.com>
2025-02-23 05:48:18 +0530

    api call add newTodo use axios and show toast message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './pages/Navbar'
 import TodoInput from './pages/TodoInput'
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (newTodo) => {
-    setTodos([...todos, newTodo]);
+  // Function to add a todo (with API call)
+  const addTodo = async (newTodo) => {
+    try {
+      const response = await axios.post("http://localhost:8000/api/v1/todo", newTodo,
+        {
+          headers: {
+            "Content-Type": "application/json"
+          },
+          withCredentials: true,
+        }
+      );
+      setTodos([...todos, response.data]); // Update UI with response data
+      toast.success("Todo added successfully! ✅"); // Success toast
+    } catch (error) {
+      toast.error("Failed to add todo ❌"); // Error toast
+      console.error("Error adding todo:", error);
+    }
   };
 
   return (
@@ -29,6 +47,9 @@ function App() {
             </li>
           ))}
         </ul>
+
+        {/* Toast Notification Container */}
+        <ToastContainer position="top-right" autoClose={2000} />
       </div>
 
     </>
